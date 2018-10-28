8df41b7c022a6ff3f0eef5a2d90bd54c69f25fad
lijinghua <598820383@qq.com>
2018-10-28 23:54:24 +0800

    update 笔记 wordcloud

--- wordcloud/pearch_heart.py
+++ wordcloud/pearch_heart.py
@@ -5,7 +5,7 @@ import jieba  # 分词库
 from wordcloud import WordCloud, ImageColorGenerator  # 词云库
 
 # 1、读入txt文本数据
-text = open(r'./src/test.txt', "r").read()
+text = open(r'./src/file.txt', "r").read()
 
 # 2、结巴分词:cut_all参数可选, True为全模式，False为精确模式,默认精确模式
 cut_text = jieba.cut(text, cut_all=False)
@@ -24,7 +24,7 @@ wc.generate(result)
 # 5、绘制文字的颜色以背景图颜色为参考
 image_color = ImageColorGenerator(graph)  # 从背景图片生成颜色值
 wc.recolor(color_func=image_color)
-wc.to_file(r"./img/pearch_heart.png")  # 按照背景图大小保存绘制好的词云图，比下面程序显示更清晰
+wc.to_file(r"./img/pear_heart.png")  # 按照背景图大小保存绘制好的词云图，比下面程序显示更清晰
 
 # 6、显示图片
 plt.figure("词云图")  # 指定所绘图名称
